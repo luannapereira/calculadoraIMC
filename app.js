fd65304d77662076d867f9ea0cc7cbfe2de4ecfa
luannapereira <93163993+luannapereira@users.noreply.github.com>
2022-12-16 17:13:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function calcular (event){
 
     peso = document.getElementById('peso').value;
     altura = document.getElementById('altura').value;
-    
+
     imc = peso / (altura * altura);
     
     resultado = document.getElementById('resultado');
@@ -35,8 +35,14 @@ function calcular (event){
     }else if (imc > 25 && imc <= 29.99){
     resultado.innerHTML = '<br> Seu resultado foi: ' 
         + imc.toFixed(2) + '<br> Você está acima do peso!'
-    }else if (imc <= 30){
+    }else if (imc > 30 && imc <= 34.99){
     resultado.innerHTML = '<br> Seu resultado foi: ' 
-        + imc.toFixed(2) + '<br> Cuidado Obeso!'
+        + imc.toFixed(2) + '<br> Obesidade Grau 1!'
+}else if (imc > 35 && imc <= 39.99){
+    resultado.innerHTML = '<br> Seu resultado foi: ' 
+        + imc.toFixed(2) + '<br> Obesidade Grau 2!'
+}else if (imc >= 40){
+    resultado.innerHTML = '<br> Seu resultado foi: ' 
+        + imc.toFixed(2) + '<br> Obesidade Grau 3 (Cuidado) !'
 }
 }
